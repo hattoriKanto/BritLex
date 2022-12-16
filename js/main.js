3e600abb0427dd56cd9141c19d1fdf6af90caf08
ibag <bahniukihor@gmail.com>
2022-12-16 15:11:04 +0200

    add pricing section; edit some js code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const itemImg = document.querySelectorAll(".item__image");                       
+const skillsItemImg = document.querySelectorAll(".skills .item__image");                       
 
-itemImg.forEach(elem =>{                                                        
-    elem.onload = function checkImg(){                                          
+skillsItemImg.forEach(elem =>{                                                        
+    elem.onload = function checkSkillsImg(){                                          
         const width = elem.naturalWidth;                                        
         const height = elem.naturalHeight;                                      
         const closestImg = elem.closest(".item__image");                  
@@ -16,7 +16,20 @@ itemImg.forEach(elem =>{
     }
 });
 
-window.onload = function myFn(){
+const pricingItemImg = document.querySelectorAll(".pricing .item__img");
+
+pricingItemImg.forEach(elem =>{                                                        
+    elem.onload = function checkpricingImg(){                                          
+        const width = elem.naturalWidth;                                        
+        const height = elem.naturalHeight;                                      
+        const closestImg = elem.closest(".item__img");                  
+        if(width !== height){                                                   
+            closestImg.classList.add("max-width");                              
+        }
+    }
+});
+
+window.onload = function checkFooterMenu(){
     const footerMenu = document.querySelector(".footer__menu");
     const footerWrapper = document.querySelector(".footer__wrapper");
     const array = footerMenu.children;
